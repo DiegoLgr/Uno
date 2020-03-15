4eb3a98111c707edc2069de20d46679031b8023e
Diegolgr <diego.lgrip@gmail.com>
2020-03-15 17:28:14 +0100

    Semicolumn lacking

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,13 @@
  *  if a path is given, it's code is run.
  *  if uno is called without arguments, an interactive RPL is launched.
  */
-
 #include "Uno.h"
 
 #include <iostream>
 
 
 int main( int argc, char *argv[]){
-    Uno uno
+    Uno uno;
     if (argc > 2){
         std::cout << "Usage: uno [script]\n";
         exit (EXIT_FAILURE);

3f8c7bd91596968146702e3ca8a2301f1532f19b
Diegolgr <diego.lgrip@gmail.com>
2020-03-15 18:30:10 +0100

    Now main handles all the logic needed to get a proper string with the source code and handles it to Uno

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,55 @@
 /**
  * @file main.cpp
- * @brief uno language interpreter.
- * @synopsis uno [path_to_scritp]
- *  if a path is given, it's code is run.
- *  if uno is called without arguments, an interactive RPL is launched.
+ *
+ * @brief Entry point for uno cli.
  */
 #include "Uno.h"
 
 #include <iostream>
+#include <fstream>
+#include <streambuf>
 
 
+void informError( void);
+void runFromFile( std::string file_path);
+void runRPL( void);
+
+
+/*
+ * @detail test validity of parameters, gets the source code and hadles it to
+ *  the Uno interpreter.
+ *  It either takes a file path to the source to be executed or nothing. in the
+ *  later case an interactive RPL is launched.
+ */
 int main( int argc, char *argv[]){
-    Uno uno;
     if (argc > 2){
-        std::cout << "Usage: uno [script]\n";
-        exit (EXIT_FAILURE);
+        informError();
     }else if (argc == 2){
-       uno.runFile( argv[1]);
+        runFromFile( argv[1]);
     }else{
-        uno.runPrompt();
+        runRPL();
+    }
+}
+
+void informError( void){
+    std::cout << "Usage: uno [script]\n";
+    std::exit (EXIT_FAILURE);
+}
+
+void runFromFile( std::string file_path){
+    Uno uno;
+    std::ifstream file( file_path);
+    std::string source((std::istreambuf_iterator<char>(file)),
+                        std::istreambuf_iterator<char>());
+    uno.run( source);
+}
+
+void runRPL( void){
+    Uno uno;
+    std::string source;
+    while( true){
+        std::cout << "Uno ~> ";
+        std::getline( std::cin, source);
+        uno.run( source);
     }
 }

e04b7e4ea9fd734d6812c338bf4583f25726fd03
DiegoLGR <diego.lgrip@gmail.com>
2020-03-25 23:41:56 +0100

    Implemented Ast with unique pointers

--- src/Parser/Ast.cpp
+++ src/Parser/Ast.cpp
@@ -4,11 +4,28 @@
 #include "Token.h"
 #include "Ast.h"
 
+class Ast {
+    private:
+        std::unique_ptr<AstNode> root;
+};
+
+class AstNode {
+    public:
+        AstNode( Token token, unique_ptr<AstNode> left_child,
+                            unique_ptr<AstNode> right_child);
+        void toString() const;
+
+    private:
+        Token token;
+        std::unique_ptr<AstNode> left_child;
+        std::unique_ptr<AstNode> right_child;
+};
+
 std::string Node::toString(){
     std::string s =" (";
-    if (child1) s = s + child1->toString();
+    if (left_childe) s = s + left_childe->toString();
     if (token.lexeme != "")  s = s + " " + token.lexeme;
-    if (child2) s = s + " " + child2->toString();
+    if (right_child) s = s + " " + right_child->toString();
     return s + ")";
 }
 

dfee48a9ec573e765f592057e6575da83f32bab2
DiegoLGR <diego.lgrip@gmail.com>
2020-03-27 22:08:04 +0100

    moved clas declaration to header

---
+++ src/Parser/AstNode.h
@@ -0,0 +1,17 @@
+#ifndef ASTNODE_H
+#define ASTNODE_H
+
+class AstNode {
+    public:
+        AstNode( Token token);
+        AstNode( Token token, std::unique_ptr<AstNode> left_child,
+                            std::unique_ptr<AstNode> right_child);
+        std::string toString( void) const;
+
+    private:
+        Token token;
+        std::unique_ptr<AstNode> left_child;
+        std::unique_ptr<AstNode> right_child;
+};
+
+#endif
